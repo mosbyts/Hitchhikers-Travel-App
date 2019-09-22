5018642a18f562a1fc12e577cd95b8e983ff4f60
Tianna Mosby <mosbyts@vcu.edu>
2019-09-22 19:17:46 -0400

    API halfway working.

--- assets/javascript/crimeScript.js
+++ assets/javascript/crimeScript.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
 // Tianna's crime API start
 $("button").on("click", function(){
-    queryURLCrime = "https://api.usa.gov/crime/fbi/sapi/api/agencies/" + location + "?api_key=JMLpV1qDeUxB4nk1hx39YC03ucTpKNh8sJxSyzKO";
+    event.preventDefault();
+    queryURLCrime = "https://api.usa.gov/crime/fbi/sapi/api/" + location + "?api_key=JMLpV1qDeUxB4nk1hx39YC03ucTpKNh8sJxSyzKO";
     $.ajax({
         url: queryURLCrime,
         method: "GET"
     }).then(function(response){
         $("#pSearch").append(response);
     });
+    var location = $("input").val("");
+    console.log(location);
 });
-
-var location = $("input").val("");
 // Tianna's crime API end
 });
