3cdb00a22d34d536707d3499c5b9562aae57189c
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-09-26 11:06:28 -0400

    inital workings of new Dark Sky weather API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,38 @@
 
 
-//START OF WEATHER API -- KEVIN ----------------------
-
-
-
-// key = 5351337fc3ab6151feda04b50a121221
-
-//rename HTML label to match line 9 tag (the submit button)
-function getWeatherForecast (cityname) {
-    var key = "5351337fc3ab6151feda04b50a121221";
-    var queryURLWeather = "api.openweathermap.org/data/2.5/forecast?q=" + cityname + ",us&mode=json"
-
-    $.ajax({
-        url: queryURLWeather,
-        method: "GET"
-    }).then(function(response){
-        $("#resultsWeather").append(response)
-        //console.log(response);
-    });
-}
-console.log(getWeatherForecast("New York"));
-
+//---------------START OF WEATHER API -- KEVIN ----------------------
+$(document).ready(function(){    
+  var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://dark-sky.p.rapidapi.com/37.5799808,-77.5274496?lang=en&units=auto",
+      "method": "GET",
+      "headers": {
+          "x-rapidapi-host": "dark-sky.p.rapidapi.com",
+          "x-rapidapi-key": "7181d116b7msh71f683ad4b0a945p12980djsn919ab6d9aae8"
+      }
+  }
+​
+  navigator.geolocation.getCurrentPosition(function(position) {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+      if(lat != null && long != null){
+          settings.url = "https://dark-sky.p.rapidapi.com/" + lat + "," + long + "?lang=en&units=auto";
+      }
+      $.ajax(settings).done(function (response) {
+          console.log(response.daily.icon);
+          console.log(response);
+      });   
+  });
   
+  $.ajax(settings).done(function (response) {
+      console.log(response.daily.icon);
+  });    
+});
 
-// END OF WEATHER API -------------------------------
+// --------------END OF WEATHER API -------------------------------
+
+
 
 
 // Tianna's Crime Script Beginning
