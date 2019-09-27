1f4718b880880fff3c75c0f3864ed2a9fed99048
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-09-27 17:23:14 -0400

    deleted old weather API code, moved to weather.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,40 +1,5 @@
 
 
-//---------------START OF WEATHER API -- KEVIN ----------------------
-$(document).ready(function(){    
-  var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://dark-sky.p.rapidapi.com/37.5799808,-77.5274496?lang=en&units=auto",
-      "method": "GET",
-      "headers": {
-          "x-rapidapi-host": "dark-sky.p.rapidapi.com",
-          "x-rapidapi-key": "7181d116b7msh71f683ad4b0a945p12980djsn919ab6d9aae8"
-      }
-  }
-​
-  navigator.geolocation.getCurrentPosition(function(position) {
-      let lat = position.coords.latitude;
-      let long = position.coords.longitude;
-      if(lat != null && long != null){
-          settings.url = "https://dark-sky.p.rapidapi.com/" + lat + "," + long + "?lang=en&units=auto";
-      }
-      $.ajax(settings).done(function (response) {
-          console.log(response.daily.icon);
-          console.log(response);
-      });   
-  });
-  
-  $.ajax(settings).done(function (response) {
-      console.log(response.daily.icon);
-  });    
-});
-
-// --------------END OF WEATHER API -------------------------------
-
-
-
-
 // Tianna's Crime Script Beginning
 $(document).ready(function(){
   // Tianna's crime API start
@@ -124,4 +89,3 @@ $("#testimonial-table > tbody").append(newRow);
 });
 
 // End of Testimonal Page
-
